013405dca3f22591d9b05cae6e8a408de408b6a0
pacmanlolo <michael.omotosho147@gmail.com>
2022-12-15 01:26:41 -0800

    Leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,4 +1,4 @@
- #include <stdio.h>
+	 #include <stdio.h>
 #include "main.h"
 
 /**
@@ -12,7 +12,11 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 100 == 0 || year % 400 == 0) && !(year % 4 == 0))
+	/**
+	 * leap year when it can be divided
+	 * by 100 and 400 or by 4 evenly
+	 */
+	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 	{
 		if (month >= 2 && day >= 60)
 		{

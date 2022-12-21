85bd558a24715b38cc1e896aa036d689926f5d8c
pacmanlolo <michael.omotosho147@gmail.com>
2022-12-21 06:15:51 -0800

    All Tasks

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ *
+ * @c: The character to print
+ *
+ * Return: On Success 1
+ * on error: -1 is returned, and error is set appriopriately.
+ */
+int _putchar(char c)

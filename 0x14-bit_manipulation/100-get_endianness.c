38693824e3163761fb106b5e0b05ebf8255070c4
pacmanlolo <michael.omotosho147@gmail.com>
2023-02-02 04:32:38 -0800

    Function that checks endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * get_endianness - checks if multibyte data is the endianness.
+ * Return: If big-endian - 0, if little-endian -1.
+ */
+int get_endianness(void)
+{
+	int number = 1;
+	char *endian = (char *)&number;
+
+	if (*endian == 1)
+		return (1);
+	return (1);
+}
